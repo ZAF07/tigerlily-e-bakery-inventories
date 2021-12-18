8fe9fb802f0ed643d69fb7598094842c2b226f2d
ZAF07 <zaffere07@gmail.com>
2021-12-18 19:19:56 +0800

    first two routes (temp)

--- api/router/router.go
+++ api/router/router.go
@@ -8,9 +8,7 @@ import (
 func Router(r *gin.Engine) *gin.Engine {
 
 	// Get all inventory
-
 	// Get all invetory by type
-
 	inventoryAPI := new(controller.InventoryAPI)
 	inventory := r.Group("inventory")
 	{
@@ -18,13 +16,6 @@ func Router(r *gin.Engine) *gin.Engine {
 		inventory.GET("/:type",inventoryAPI.GetInventoryByType)
 	}
 
-	// bakeryAPI := new(controller.BakeryAPI)
-	// bakery := r.Group("bakery")
-	// {
-	// 	// bakery.GET("/", bakeryAPI.GetAllPastries)
-	// 	bakery.GET("/inventory", bakeryAPI.GetAllPastries)
-		
-	// }
 
 	checkOut := r.Group("checkout")
 	checkoutAPI := new(controller.CheckoutAPI)
