ce91b51d689fe061f51c156619ce5442e1261ee6
ZAF07 <zaffere07@gmail.com>
2022-05-14 18:20:02 +0800

    updated rpc

--- gen_rpc.sh
+++ gen_rpc.sh
@@ -6,7 +6,7 @@ protoc --go_out=api/rpc --go_opt=paths=source_relative --go-grpc_out=api/rpc  --
 echo "Copying into api/rpc"
 cp -R api/rpc/proto/inventory/* api/rpc
 
-# echo "Cleaning api/rpc directory"
-# rm -rf api/rpc/proto
+echo "Cleaning api/rpc directory"
+rm -rf api/rpc/proto
 
 echo "DONE"
