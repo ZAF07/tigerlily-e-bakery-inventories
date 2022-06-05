8e3d00024450ee76fea22ba2eb0587516f3591cc
ZAF07 <zaffere07@gmail.com>
2022-06-05 17:46:18 +0800

    Updating submodules

--- gen_rpc.sh
+++ gen_rpc.sh
@@ -8,10 +8,10 @@ protoc --go_out=api/rpc --go_opt=paths=source_relative --go-grpc_out=api/rpc  --
 echo "Copying into api/rpc.."
 cp -R api/rpc/proto/inventory/* api/rpc
 
-echo "Commtting code so that cahnges will reflect.."
-git commit -am 'Updating submodules'
-
 echo "Cleaning api/rpc directory.."
 rm -rf api/rpc/proto
+
+echo "Commtting code so that cahnges will reflect.."
+git commit -am 'Updating submodules'
 
 echo "DONE"
